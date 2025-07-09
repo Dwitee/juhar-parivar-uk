64599f09809392a99ef9bc4d525f657a3a648879
Dwitee Krishna Panda <dwitee@gmail.com>
2025-07-09 11:09:16 +0100

    Update checkout.ts

--- nuakhai-event/pages/api/checkout.ts
+++ nuakhai-event/pages/api/checkout.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2024-04-10',
+  apiVersion: '2022-11-15',
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
