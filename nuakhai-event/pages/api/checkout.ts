555fddd2485c131014e594b268a711dd5544e4c7
Dwitee Krishna Panda <dwitee@gmail.com>
2025-07-10 21:29:36 +0100

    Update checkout.ts

--- nuakhai-event/pages/api/checkout.ts
+++ nuakhai-event/pages/api/checkout.ts
@@ -30,7 +30,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
 
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card', 'paypal'],
+        payment_method_types: ['card'],
         line_items: [{
           price_data: {
             currency: 'gbp',
